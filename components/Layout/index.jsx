d2bd5736e89914c6b365f5180415e0b94775b6b1
Tramsi <tramsi2014@gmail.com>
2020-07-18 23:16:54 +0200

    Tested homepage and added storybook component

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 const Layout = ({ children }) => {
   return (
     <>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,11 @@
-export const Home = () => {
-  return "Elhaw";
+const Home = () => {
+  return <div className="text-ultra text-c200">Hello World</div>;
 };
 
-export async function getServerSideProps(contexts) {
+export async function getServerSideProps(context) {
   return {
     props: {},
   };
 }
+
+export default Home;
