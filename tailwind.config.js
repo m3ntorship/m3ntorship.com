272b1b84686fb7b1074f5651ed15dc247d17f6da
elhaw <abd.elsayed@hotmail.com>
2020-07-19 12:46:04 +0400

    added config for button paddgin

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,7 @@ module.exports = {
         c800: "rgba(0, 0, 0, 0.96)",
         c900: "#0A0A0A",
         c1000: "rgba(238, 239, 240, 0.2)",
+        c1100 :"rgba(196, 196, 196, 0.29)"
       },
       fontSize: {
         xxs: "0.875rem", //14px
@@ -37,6 +38,14 @@ module.exports = {
         super: "4.375rem", //70px
         ultra: "5rem", //80px
         giant: "7.75rem", //124px
+      },
+      spacing: {
+        smBtn: "1.5rem 2.5rem", // 16px 40px
+        baseBtn: "1.5rem 3.5rem", // 16px 56px
+        mdBtn: " 1.5rem 4rem", // 16px 64px
+        lgBtn: "1.5rem 4.5rem", // 16px 72px
+        vlgBtn: "1.5rem 6.5rem", // 16px 104px
+        giantBtn: "1.5rem 18rem"
       },
       lineHeight: {
         body: "1.2",
