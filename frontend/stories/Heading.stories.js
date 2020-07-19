9862c16e5cc403bbce3501261f217a619fd3a2db
Tramsi <tramsi2014@gmail.com>
2020-07-19 22:33:07 +0200

    Used knobs in heading story

--- frontend/stories/Heading.stories.js
+++ frontend/stories/Heading.stories.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Heading } from '../components/shared/Heading';
+import { Heading, GradientText } from '../components/shared/Heading';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 export default {
   title: 'Heading',
-  component: Heading
+  component: Heading,
+  decorators: [withKnobs]
 };
+const headingText = text('Heading Text', 'Hello World');
 export const HeadingComponent = () => {
   return (
-    <Heading
-      primaryText={'hello'}
-      gradientText={'World'}
-      gradientColor={'blue'}
-    />
+    <Heading type={text('type', 'main')}>
+      {headingText}{' '}
+      <GradientText
+        text={text('gradient text', 'Colored Text')}
+        color={text('corlor', 'blue')}
+      />
+    </Heading>
   );
 };
