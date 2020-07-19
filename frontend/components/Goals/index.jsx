4f602dd2bf480acb33d5af9a9fa47b88a8b861c9
malngaawy <mohager477@gmail.com>
2020-07-20 00:11:46 +0200

    add side img and some stylies

--- frontend/components/Goals/index.jsx
+++ frontend/components/Goals/index.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import image from './images/Squiggles Monochromatic.png';
+import './style.css';
 
 const Goals = ({ header, goals }) => {
   return (
-    <div className="text-center my-10 text-lg">
-      <h2 className="my-2 uppercase font-bold text-c100 mb-5">{header}</h2>
-      {goals.map(goal => {
-        return <p className="uppercase mb-12">{goal}</p>;
-      })}
+    <div className="relative w-full">
+      <div className="text-center my-10 text-lg container">
+        <div className="absolute graph">
+          <img src={image} />
+        </div>
+        <h2 className="my-2 uppercase font-bold text-c100 mb-5 title">
+          {header}
+        </h2>
+        <div className="mt-16">
+          {goals.map(goal => {
+            return <p className="uppercase mb-12 font-normal">{goal}</p>;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
