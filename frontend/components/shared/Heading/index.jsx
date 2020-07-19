78ed7791213a86f202111b8a8d5adf56973b9810
Tramsi <tramsi2014@gmail.com>
2020-07-19 22:32:29 +0200

    Added GradientText Component

--- frontend/components/shared/Heading/index.jsx
+++ frontend/components/shared/Heading/index.jsx
@@ -1,34 +1,39 @@
 import React from 'react';
 import cn from 'classnames';
 
-export const Heading = ({ primaryText, gradientText, type, gradientColor }) => {
+export const Heading = ({ children, type }) => {
   return (
     <span
       className={cn('font-bold leading-remove inline-block', {
         //Text Size
-        'text-super': !type ?? type === 'main',
-        'text-xxxl': type === 'secondary',
-        'text-xxl': type === 'section',
+        'text-super': !type || type === 'main',
+        'text-xxxlg': type === 'secondary',
+        'text-xxlg': type === 'section',
         'text-lg': type === 'card',
         'text-md': type === 'cardSmall',
         'text-ultra': type === 'mainLarge'
       })}
     >
-      {primaryText}{' '}
-      <span
-        className={cn(
-          'heading__secondary-text leading-remove inline-block relative '
-        )}
-        style={{
-          background: `linear-gradient(0deg, ${
-            gradientColor === 'blue'
-              ? 'rgba(45, 91, 255,1)'
-              : 'rgba(102,242,141,1)'
-          } 0%,rgba(0,0,0,0) 40%)`
-        }}
-      >
-        {gradientText}
-      </span>
+      {children}
+    </span>
+  );
+};
+
+export const GradientText = ({ text, gradientColor }) => {
+  if (!text) {
+    return null;
+  }
+  return (
+    <span
+      className={cn(
+        'heading__secondary-text leading-remove inline-block relative ',
+        {
+          'gradient-green': !gradientColor || gradientColor === 'green',
+          'gradient-blue': gradientColor === 'blue'
+        }
+      )}
+    >
+      {text}
     </span>
   );
 };
