80d7632bad3eae0fe241bbf6fc734fd029719e7f
malngaawy <mohager477@gmail.com>
2020-07-20 16:49:03 +0200

    use shared heading component

--- frontend/components/Goals/index.jsx
+++ frontend/components/Goals/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import image from './images/Squiggles Monochromatic.png';
+import { GradientText } from '../shared/Heading';
 import '../../styles/goals.css';
 
 const Goals = ({ data }) => {
@@ -11,9 +12,7 @@ const Goals = ({ data }) => {
           <div className="absolute graph opacity-25 md:opacity-100">
             <img src={image} />
           </div>
-          <h2 className="my-2 uppercase font-bold text-c100 mb-5 title">
-            {title}
-          </h2>
+          <GradientText text={title} gradientColor="green" className="bold" />
           <div className="mt-16">
             {goals.map(goal => {
               return <p className="uppercase mb-12 font-normal">{goal}</p>;
