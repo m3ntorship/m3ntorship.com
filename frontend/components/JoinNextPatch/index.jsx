7f956ac4beaee0f289738e15431d0072f1bc32b0
Alaa <Alaa.Saedy.eg@gmail.com>
2020-07-21 00:39:55 +0200

    fixing spacing

--- frontend/components/JoinNextPatch/index.jsx
+++ frontend/components/JoinNextPatch/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Heading } from '../shared/Heading';
 
 const JoinNextPatch = ({ data }) => {
   const { title, description } = data;
   return (
-    <div className="join-next-patch border-4 border-dashed border-c100 w-3/4 h-1/2 m-auto">
-      <h6 className="text-lg text-center my-16 font-bold uppercase">{title}</h6>
+    <div className="join-next-patch border-4 border-dashed border-c100">
+      <Heading type="card"> Join Next Patch</Heading>
+
       <p className="text-center my-8 px-2 text-xs font-normal">{description}</p>
 
       <img
