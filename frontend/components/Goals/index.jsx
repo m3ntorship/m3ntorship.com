16cdae1144ecd543f2db0f4afc1189210e52f4fa
malngaawy <mohager477@gmail.com>
2020-07-20 18:39:17 +0200

    edit handle data to match the content type

--- frontend/components/Goals/index.jsx
+++ frontend/components/Goals/index.jsx
@@ -4,18 +4,26 @@ import { GradientText } from '../shared/Heading';
 import '../../styles/goals.css';
 
 const Goals = ({ data }) => {
-  const { title, goals } = data;
+  const {
+    title,
+    goals_lists,
+    side_image: { url }
+  } = data;
   if (data) {
     return (
       <div className="relative w-full">
         <div className="text-center my-10 text-lg container">
           <div className="absolute graph opacity-25 md:opacity-100">
-            <img src={image} />
+            <img src={url} />
           </div>
           <GradientText text={title} gradientColor="green" className="bold" />
           <div className="mt-16">
-            {goals.map(goal => {
-              return <p className="uppercase mb-12 font-normal">{goal}</p>;
+            {goals_lists.map(({ id, text }) => {
+              return (
+                <p className="uppercase mb-12 font-normal" key={id}>
+                  {text}
+                </p>
+              );
             })}
           </div>
         </div>
--- frontend/stories/Goals.stories.js
+++ frontend/stories/Goals.stories.js
@@ -5,11 +5,21 @@ import { withKnobs, boolean } from '@storybook/addon-knobs';
 export default { title: 'Goals', component: Goals, decorators: [withKnobs] };
 
 const data = {
-  title: 'Goals',
-  goals: [
-    'Help new developers gain real world experience',
-    'Help NGOs and small businesses build their online identity'
-  ]
+  side_image: {
+    url:
+      'https://res.cloudinary.com/malngaawy/image/upload/v1595261682/Squiggles_Monochromatic_jwvct9.png'
+  },
+  goals_lists: [
+    {
+      text: 'Help new developers gain real world experience',
+      id: '5f15be11c8184a2250a760e2'
+    },
+    {
+      text: 'Help NGOs and small businesses build their online identity',
+      id: '5f15be24c8184a2250a760e3'
+    }
+  ],
+  title: 'Goals'
 };
 
 export const GoalsComponent = () => {
