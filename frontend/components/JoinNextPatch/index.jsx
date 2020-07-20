4aabf4e88980e34df75161fa1f708c7fbbb10afd
Alaa <Alaa.Saedy.eg@gmail.com>
2020-07-20 23:16:19 +0200

    fixing spacing

--- frontend/components/JoinNextPatch/index.jsx
+++ frontend/components/JoinNextPatch/index.jsx
@@ -3,12 +3,15 @@ import './style.css';
 
 const JoinNextPatch = ({ data }) => {
   return (
-    <div className="join-next-patch border-4 border-dashed border-c100">
-      <h6 className="text-lg text-center mt-16 font-bold uppercase">
+    <div className="join-next-patch border-4 border-dashed border-c100 w-3/4 h-1/2 m-auto">
+      <h6 className="text-lg text-center my-16 font-bold uppercase">
         {data.title}
       </h6>
-      <p className="text-center mt-6 text-xs font-normal">{data.description}</p>
-      <img src="../" alt="" />
+      <p className="text-center my-8 px-2 text-xs font-normal">
+        {data.description}
+      </p>
+      <img src="../public/Squiggles Colorful-1.png" alt="" />
+      <img src="../public/Squiggles Colorful-2.png" alt="" />
     </div>
   );
 };
