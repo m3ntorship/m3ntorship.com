b9663780af1bd19b05373e480bb13c2b2474ea60
Alaa <Alaa.Saedy.eg@gmail.com>
2020-07-21 01:53:06 +0200

    use of heading component, adding img srcs as data

--- frontend/components/JoinNextPatch/index.jsx
+++ frontend/components/JoinNextPatch/index.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import { Heading } from '../shared/Heading';
 
 const JoinNextPatch = ({ data }) => {
-  const { title, description } = data;
+  const { title, description, left_img, right_img } = data;
   return (
-    <div className="join-next-patch border-4 border-dashed border-c100">
-      <Heading type="card"> Join Next Patch</Heading>
-
-      <p className="text-center my-8 px-2 text-xs font-normal">{description}</p>
-
-      <img
-        src="../../public/static/resources/Squiggles Colorful-1.png"
-        alt=""
-      />
+    <div className="join-next-patch border-4 border-dashed border-c100 flex justify-around">
+      <div className="my-24">
+        <img className="w-56 h-64" src={left_img} alt="" />
+      </div>
+      <div>
+        <span className="block text-center mt-16 py-5 uppercase">
+          <Heading type="cardSmall">Join Next Patch</Heading>
+        </span>
+        <p className="text-center px-2 py-8 text-xs font-normal">
+          {description}
+        </p>
+      </div>
+      <div className="my-24">
+        <img className="w-56 h-64" src={right_img} alt="" />
+      </div>
     </div>
   );
 };
