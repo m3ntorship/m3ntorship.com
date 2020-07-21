65bc40bf6a02d156986c73af3b613a7fe2e087f9
Tramsi <tramsi2014@gmail.com>
2020-07-21 18:34:25 +0200

    Added the ability to add custom classes to heading and gradient text component

--- frontend/components/shared/Heading/index.jsx
+++ frontend/components/shared/Heading/index.jsx
@@ -1,34 +1,41 @@
 import React from 'react';
 import cn from 'classnames';
 
-export const Heading = ({ children, type }) => {
+export const Heading = ({ children, type, className }) => {
   return (
     <span
-      className={cn('font-bold', {
-        //Text Size
-        'text-super': !type || type === 'main',
-        'text-xxxlg': type === 'secondary',
-        'text-xxlg': type === 'section',
-        'text-lg': type === 'card',
-        'text-md': type === 'cardSmall',
-        'text-ultra': type === 'mainLarge'
-      })}
+      className={cn(
+        'font-bold',
+        {
+          //Text Size
+          'text-super': !type || type === 'main',
+          'text-xxxlg': type === 'secondary',
+          'text-xxlg': type === 'section',
+          'text-lg': type === 'card',
+          'text-md': type === 'cardSmall',
+          'text-ultra': type === 'mainLarge'
+        },
+        className
+      )}
     >
       {children}
     </span>
   );
 };
 
-export const GradientText = ({ text, gradientColor }) => {
+export const GradientText = ({ text, gradientColor, className }) => {
   if (!text) {
     return null;
   }
   return (
     <span
-      className={cn({
-        'gradient-green': !gradientColor || gradientColor === 'green',
-        'gradient-blue': gradientColor === 'blue'
-      })}
+      className={cn(
+        {
+          'gradient-green': !gradientColor || gradientColor === 'green',
+          'gradient-blue': gradientColor === 'blue'
+        },
+        className
+      )}
     >
       {text}
     </span>
