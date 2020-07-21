b2eb694d28eb2341a9bdb703d0d74a3a9f3bcc36
AbdoMagdy <abdoman46@hotmail.com>
2020-07-21 17:44:17 +0200

    add mock data to the componenet story

--- frontend/components/join us/index.jsx
+++ frontend/components/join us/index.jsx
@@ -2,20 +2,18 @@ import React from 'react';
 import { Heading, GradientText } from '../shared/Heading/index';
 import '../../styles/joinUs.css';
 const JoinUs = ({ data }) => {
+  let { primaryHeading, secondaryHeading, description } = data;
   return (
     <div className="grid grid-cols-2 p-10">
       <div className="mx-6">
         <div className="JoinUs__dots float-right"></div>
         <div className="mb-4 leading-normal">
           <Heading type="main">
-            {'One More Step To'}{' '}
-            <GradientText text="Join Us" gradientColor="blue" />
+            {primaryHeading}{' '}
+            <GradientText text={secondaryHeading} gradientColor="blue" />
           </Heading>
         </div>
-        <p className="text-c700 text-md">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie
-          aliquam iaculis facilisis
-        </p>
+        <p className="text-c700 text-md">{description}</p>
       </div>
       <div className="JoinUs__right"></div>
     </div>
--- frontend/stories/JoinUs.stories.js
+++ frontend/stories/JoinUs.stories.js
@@ -2,11 +2,17 @@ import React from 'react';
 import JoinUs from '../components/join us/index';
 import { withKnobs, text } from '@storybook/addon-knobs';
 
+const data = {
+  primaryHeading: 'ONE MORE STEP TO',
+  secondaryHeading: 'JOIN US',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie aliquam iaculis facilisis'
+};
 export default {
   title: 'Join Us',
   component: JoinUs,
   decorators: [withKnobs]
 };
 export const JoinUsComponent = () => {
-  return <JoinUs />;
+  return <JoinUs data={data} />;
 };
