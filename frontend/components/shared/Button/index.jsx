2199528e06c188051a3f94e482bbcf87d602e6cd
elhaw <abd.elsayed@hotmail.com>
2020-07-21 18:33:33 +0400

    adding border props

--- frontend/components/shared/Button/index.jsx
+++ frontend/components/shared/Button/index.jsx
@@ -1,52 +1,50 @@
-import React from "react";
+import React from 'react';
 
-import cn from "classnames";
+import cn from 'classnames';
 
 const Button = ({ children, ...props }) => {
   let {
-    primaryTextColor,
-    fontSize,
-    primaryClassName,
-    secondaryClassName,
+    customClassName,
+    textColor,
     bgColor,
     btnPadding,
-    bntText,
-    btnUrl,
+    borderColor,
+    borderStyle
   } = props;
   return (
     <>
       <a
         className={cn(
-          "extra-bold",
+          'extra-bold text-base inline-block',
           {
-            // sizes
-            "text-sm": fontSize === "sm",
-            "text-xs": fontSize === "xs",
-            "text-base": !fontSize,
-            "text-md": fontSize === "md",
+            // border
+            'border-none': !borderStyle,
+            'border border-solid': borderStyle === 'solid',
+            'border-c300': borderColor === 'blue',
+            'border-c200': borderColor === 'green',
+            'border-c100': borderColor === 'black',
 
             // colors
-            "text-c100": !primaryTextColor,
-            "text-c000": primaryTextColor === "white",
-            "text-c1000": primaryTextColor === "c1000",
-            "text-c800": primaryTextColor === "gray",
+            'text-c100': !textColor,
+            'text-c000': textColor === 'white',
+            'text-c1000': textColor === 'gray',
+            'text-c200': textColor === 'green',
 
             //bg Color
-            "bg-c000": !bgColor,
-            "bg-c100": bgColor === "black",
-            "bg-c200": bgColor === "green",
-            "bg-c300": bgColor === "blue",
-            "bg-c1100": bgColor === "c1100",
+            'bg-c000': !bgColor,
+            'bg-c100': bgColor === 'black',
+            'bg-c200': bgColor === 'green',
+            'bg-c300': bgColor === 'blue',
+            'bg-c1000': bgColor === 'gray',
 
             //spacing
-            "p-baseBtn": !btnPadding,
-            "p-smBtn": btnPadding === "smBtn",
-            "p-mdBtn": btnPadding === "mdBtn",
-            "p-lgBtn": btnPadding === "lgBtn",
-            "p-vlgBtn": btnPadding === "vlgBtn",
+            'p-baseBtn': !btnPadding,
+            'p-smBtn': btnPadding === 'small',
+            'p-mdBtn': btnPadding === 'medium',
+            'p-lgBtn': btnPadding === 'large',
+            'p-vlgBtn': btnPadding === 'verylarge'
           },
-          primaryClassName,
-          secondaryClassName
+          customClassName
         )}
       >
         {children}
