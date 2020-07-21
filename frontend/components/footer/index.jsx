d388075b87a45519168968aa4b1cd4f8ec8b5951
AbdoMagdy <abdoman46@hotmail.com>
2020-07-21 17:33:57 +0200

    handle data error

--- frontend/components/footer/index.jsx
+++ frontend/components/footer/index.jsx
@@ -3,42 +3,46 @@ import '../../styles/footer.css';
 
 const Footer = ({ data }) => {
   const { links, about, copyrights } = data;
-  return (
-    <footer className="p-10">
-      <div className="grid grid-cols-3">
-        <div className="mx-4 relative">
-          <div className="footer__copyrights__bg absolute xl:block float-right w-16 h-16"></div>
-          <h2 className="font-bold mb-8 text-lg">M3ntorship</h2>
-          <p className="text-xxs text-c700 leading-5">
-            {' '}
-            {copyrights}
-            <br /> All rights reserved.{' '}
-          </p>
+  if (data) {
+    return (
+      <footer className="p-10">
+        <div className="grid grid-cols-3">
+          <div className="mx-4 relative">
+            <div className="footer__copyrights__bg absolute xl:block float-right w-16 h-16"></div>
+            <h2 className="font-bold mb-8 text-lg">M3ntorship</h2>
+            <p className="text-xxs text-c700 leading-5">
+              {' '}
+              {copyrights}
+              <br /> All rights reserved.{' '}
+            </p>
+          </div>
+          <div className="mx-4">
+            <h3 className="text-base mb-8 font-bold">About us</h3>
+            <p className="text-c700">
+              {about.text.slice(0, 120).concat('... ')}
+              <a className="underline" href={about.url}>
+                Read More
+              </a>
+            </p>
+          </div>
+          <div className="mx-4">
+            <h3 className="text-base mb-6 font-bold">Links</h3>
+            <ul className="flex flex-col flex-wrap h-32">
+              {links.map(link => {
+                return (
+                  <li className="mb-4 leading-7">
+                    <a href={link.url}>{link.text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-        <div className="mx-4">
-          <h3 className="text-base mb-8 font-bold">About us</h3>
-          <p className="text-c700">
-            {about.text.slice(0, 120).concat('... ')}
-            <a className="underline" href={about.url}>
-              Read More
-            </a>
-          </p>
-        </div>
-        <div className="mx-4">
-          <h3 className="text-base mb-6 font-bold">Links</h3>
-          <ul className="flex flex-col flex-wrap h-32">
-            {links.map(link => {
-              return (
-                <li className="mb-4 leading-7">
-                  <a href={link.url}>{link.text}</a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </div>
-    </footer>
-  );
+      </footer>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default Footer;
