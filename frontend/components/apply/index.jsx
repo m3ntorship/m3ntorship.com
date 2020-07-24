ff5c2ee6c2672c83eb619d01e3aca28c30ed890d
AbdoMagdy <abdoman46@hotmail.com>
2020-07-24 19:32:13 +0200

    initial apply componenet

---
+++ frontend/components/apply/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Heading, GradientText } from '../shared/Heading/index';
+
+const Apply = () => {
+  return (
+    <div>
+      <Heading>
+        You Want to <GradientText text="Apply As" gradientColor="blue" />
+          </Heading>
+          
+    </div>
+  );
+};
+
+export default Apply;
