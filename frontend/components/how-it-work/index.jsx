530ce4a52ebd2758bd33ac277a7c9dd702a57980
Alngaawy <m.alngaawy@gmail.com>
2020-07-25 20:00:10 +0200

    use Heading Component the right way

--- frontend/components/how-it-work/index.jsx
+++ frontend/components/how-it-work/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GradientText } from '../shared/Heading';
+import { GradientText, Heading, HEADING_OPTIONS } from '../shared/Heading';
 import PersonCard from '../person-card';
 
 const HowItWork = ({ title, paragraph, cardDetails, sideImage, steps }) => {
@@ -9,9 +9,9 @@ const HowItWork = ({ title, paragraph, cardDetails, sideImage, steps }) => {
         <div className="absolute left-0 top-0 lg:-ml-10 opacity-25 md:opacity-100">
           <img src={sideImage.url} />
         </div>
-        <div>
-          <GradientText text={title} className="text-xxlg uppercase font-bold" />
-        </div>
+        <Heading type={HEADING_OPTIONS.TYPE.SECTION} textAlign={HEADING_OPTIONS.TEXT_ALIGN.CENTER} >
+          <GradientText text={title} className="uppercase font-bold" />
+        </Heading>
         <p className="my-10 mx-auto text-base text-c600 lg:w-4/6">{paragraph}</p>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
           {steps.map((el) => {
