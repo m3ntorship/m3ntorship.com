aecb1789c54b4d269be37d39acc755213d4534e0
Alngaawy <m.alngaawy@gmail.com>
2020-07-25 14:51:37 +0200

    edit some styles in persom card

--- frontend/components/person-card/index.js
+++ frontend/components/person-card/index.js
@@ -23,14 +23,14 @@ const PersonCard = ({ cardDetails, bgColord, rounded, boxShadow }) => {
       {cardDetails && (
         <div
           style={{ boxShadow: boxShadow && '0 0 40px rgba(0, 0, 0, 0.1)' }}
-          className={cn('card', 'overflow-hidden', 'px-4', 'py-8', {
+          className={cn('card', 'overflow-hidden', 'px-10', 'py-10', {
             'bg-c400': bgColord,
             'text-center': rounded
           })}
         >
           {image && (
             <div
-              className={cn('card__image', 'mx-auto', 'pb-4', {
+              className={cn('person__card__image', 'mx-auto', 'pb-4', 'text-center', {
                 'w-56': !rounded,
                 'w-32': rounded
               })}
@@ -38,25 +38,25 @@ const PersonCard = ({ cardDetails, bgColord, rounded, boxShadow }) => {
               <img
                 src={image}
                 alt="title"
-                className={cn({ 'rounded-full': rounded })}
+                className={cn({ 'rounded-full w-20 h-20 max-w-none m-auto': rounded })}
                 style={{ objectFit: 'cover' }}
               />
             </div>
           )}
           {title && (
-            <p className="card__title text-c800 font-bold text-md pb-4 text-center">
+            <p className="card__title text-c800 font-bold text-lg pb-4 text-center">
               {title}
             </p>
           )}
           {subtitle && (
-            <p className="card__subtitle pb-2 text-center text-base text-c700">
+            <p className="card__subtitle pb-2 text-center text-sm font-light text-c500">
               {subtitle}
             </p>
           )}
 
           {description && (
             <p
-              className={cn('card__description', 'text-sm', 'pb-2', {
+              className={cn('card__description', 'text-xs', 'pb-2', 'font-normal', 'my-5', {
                 'text-c600': !rounded,
                 'text-c800': rounded
               })}
