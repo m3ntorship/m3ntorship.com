fcaaeb5fa1c72a201103d360a4193ed68682823b
Alngaawy <m.alngaawy@gmail.com>
2020-07-25 14:51:12 +0200

    create team component

---
+++ frontend/components/Team/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {GradientText} from '../shared/Heading';
+import PersonCard from '../person-card'
+
+export const Team = ({data}) => {
+    if(data) {
+        const {title, side_image:{url}, paragraph, team_members} = data
+        return (
+        <div className='team relative'>
+            <div className='text-center my-10 container'>
+            <div className="absolute graph hidden md:block">
+                <img src={url}/>
+                </div>
+                <div className='text-center'>
+          <GradientText text={title} className="uppercase font-bold text-xxlg inline-block"  />
+            </div>
+          <p className='my-10 mx-auto text-center text-xs md:text-base text-c600 lg:w-4/6'>{paragraph}</p>
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
+              {team_members.map(member => <PersonCard cardDetails={member} bgColord={true} rounded={true} /> )}
+          </div>
+            </div>
+        </div>
+        )
+    }
+}
