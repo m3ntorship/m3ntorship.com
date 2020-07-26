f9fd22dd2970572fb6dfeed2727b79630f8f06f2
Alngaawy <m.alngaawy@gmail.com>
2020-07-26 16:33:36 +0200

    hide side image in tablet screen

--- frontend/components/Team/index.jsx
+++ frontend/components/Team/index.jsx
@@ -8,15 +8,15 @@ export const Team = ({data}) => {
         return (
         <div className='team relative'>
             <div className='text-center my-10 container'>
-            <div className="absolute graph hidden md:block">
+            <div className="absolute graph hidden lg:block">
                 <img src={url}/>
                 </div>
-                <div className='text-center'>
+            <div className='text-center'>
           <GradientText text={title} className="uppercase font-bold text-xxlg inline-block"  />
             </div>
           <p className='my-10 mx-auto text-center text-xs md:text-base text-c600 lg:w-4/6'>{paragraph}</p>
           <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
-              {team_members.map(member => <PersonCard cardDetails={member} bgColord={true} rounded={true} /> )}
+              {team_members.map((member, index) => <PersonCard key={index} cardDetails={member} bgColord={true} rounded={true} /> )}
           </div>
             </div>
         </div>
