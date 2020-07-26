4494e9f7cd9b5bc554f7bc9e8998fc7693c173df
Alaa <Alaa.Saedy.eg@gmail.com>
2020-07-26 18:51:10 +0200

    change heading custom property, center side images

--- frontend/components/JoinUsComponent/index.jsx
+++ frontend/components/JoinUsComponent/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Heading } from '../shared/Heading';
+import { Heading, HEADING_OPTIONS } from '../shared/Heading';
 import Button from '../shared/Button';
 
 const JoinUs = ({ data }) => {
@@ -16,18 +16,18 @@ const JoinUs = ({ data }) => {
       <div className="flex justify-around mx-0 lg:mx-32">
         <div className="my-24 w-1/6">
           <img
-            className="object-cover w-11/12 hidden md:block"
+            className="object-cover w-11/12 flex items-center hidden md:block"
             src={left_img}
             alt=""
           />
         </div>
         <div className="w-4/6 flex flex-col justify-center mx-auto mb-24">
           <Heading
-            type="section"
-            textAlign="center"
-            textTransform="uppercase"
-            as="h1"
+            type={HEADING_OPTIONS.TYPE.SECTION}
+            textAlign={HEADING_OPTIONS.TEXT_ALIGN.CENTER}
+            textTransform={HEADING_OPTIONS.TEXT_TRANSFORM.UPPERCASE}
             className="mt-16 pb-5"
+            as="h3"
           >
             {title}
           </Heading>
@@ -54,7 +54,7 @@ const JoinUs = ({ data }) => {
             {mentorBtn}
           </Button>
         </div>
-        <div className="my-24 w-1/6">
+        <div className="my-24 w-1/6 flex items-center">
           <img
             className="object-cover w-11/12 hidden md:block"
             src={right_img}
