e47acae5e299b263826050ff667b693017480289
MAlngaawy <m.alngaawy@gmail.com>
2020-07-27 13:09:10 +0200

    add ptches component and chenge images json data

--- frontend/components/HomePage/index.jsx
+++ frontend/components/HomePage/index.jsx
@@ -5,16 +5,18 @@ import SectionHeader from '../shared/SectionHeader';
 import Button from '../shared/Button';
 import Goals from '../Goals';
 import HowItWork from '../how-it-work';
+import Patches from '../Patches'
 
 export const HomePage = ({data}) => {
     if(data) {
-        const {top_bar_data, section_header_data, goals_data, how_it_working_data} = data
+        const {top_bar_data, section_header_data, goals_data, how_it_working_data, patches_data} = data
         return (
             <>
             <TopBar data={top_bar_data} />
             <SectionHeaderComponent data={section_header_data} />
             <Goals data={goals_data} />
             <HowItWork data={how_it_working_data} />
+            <Patches data={patches_data} />
             </>
         )
     }
--- frontend/stories/HomePage.stories.js
+++ frontend/stories/HomePage.stories.js
@@ -9,6 +9,16 @@ export default {
     component: HomePage
 }
 
+const imagesData = [
+    'https://s3.amazonaws.com/pixpa.com/com/articles/1525891879-683036-peter-sjo-201640-unsplashjpg.png',
+    'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9',
+    'https://www.worldphoto.org/sites/default/files/default-media/Piercy.jpg',
+    'https://s3.amazonaws.com/pixpa.com/com/articles/1525891879-379720-warren-wong-242286-unsplashjpg.png',
+    'https://the-pro-photographer.com/wp-content/uploads/2016/09/Portrait-photography-settings%E2%80%AC.jpeg',
+    'https://www.adobe.com/content/dam/cc/us/en/creative-cloud/photography/discover/portrait-photography/CODERED_B1_portrait_photography-P4b_438x447.jpg.img.jpg',
+    'https://4691df0fe49ad0936487-e0121cbc87e93da04e06616472138769.ssl.cf1.rackcdn.com/6-copy4.jpg'
+]
+
 const data = {
     top_bar_data: {
         logo_title: 'M3ntorship',
@@ -105,7 +115,42 @@ const data = {
             }
           },
         ]
-      }
+    },
+    patches_data: {
+        title: 'Patches',
+        paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie aliquam iaculis facilisis sit pharetra pellentesque ullamcorper.',
+        cards: [
+            {
+                cardDetails: {
+                    title: 'VOLUME 1',
+                    images: imagesData,
+                    description: 'A job listings and job application app for working abroad.',
+                    anchorText: 'View team'
+                }
+            },
+            {
+                cardDetails: {
+                    title: 'VOLUME 2',
+                    images: imagesData,
+                    description: 'A job listings and job application app for working abroad.',
+                    anchorText: 'View team'
+                }
+            },
+            {
+                cardDetails: {
+                    title: 'VOLUME 3',
+                    images: imagesData,
+                    description: 'A job listings and job application app for working abroad.',
+                    anchorText: 'View team'
+                }
+            }
+        ],
+        dashedCardDetails: {
+            title: 'Join Next Patch',
+            description: 'A job listings and job application app for working abroad.',
+            anchorText: 'APPLY NOW'
+        }
+    }
 }
 
 export const HomePageReview = () => {
