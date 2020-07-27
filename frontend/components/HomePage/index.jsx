21543ae1190ff3d0ed8c3551d2e0e61b8df2e036
MAlngaawy <m.alngaawy@gmail.com>
2020-07-27 11:31:44 +0200

    add section header component to homepage component

--- frontend/components/HomePage/index.jsx
+++ frontend/components/HomePage/index.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 // Import Home Page Components
-import {TopBar} from '../TopBar'
+import {TopBar} from '../TopBar';
+import SectionHeader from '../shared/SectionHeader';
+import Button from '../shared/Button'
 
 export const HomePage = ({data}) => {
     if(data) {
-        const {top_bar_data} = data
+        const {top_bar_data, section_header_data} = data
         return (
             <>
             <TopBar data={top_bar_data} />
+            <SectionHeaderComponent data={section_header_data} />
             </>
         )
     }
+}
+
+// side components
+const SectionHeaderComponent = ({data}) => {
+    return (
+        <div className='container'>
+        <SectionHeader data={data}> 
+            <Button textColor='black' bgColor='green' btnPadding='small' textSize='medium' customClassName='uppercase' > Apply As A Member </Button>
+            <Button textColor='white' bgColor='blue' btnPadding='small' textSize='medium' customClassName='uppercase ml-2' > Apply As A Mentor </Button>
+        </SectionHeader>
+        </div>
+    )
 }
--- frontend/stories/HomePage.stories.js
+++ frontend/stories/HomePage.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HomePage } from '../components/HomePage';
+import right_image from '../components/HomePage/images/right_image.png';
 
 export default {
     title: 'Home Page',
@@ -14,6 +15,13 @@ const data = {
           link_url: '/apply',
           link_text: 'Apply Now'
         } 
+    },
+    section_header_data: {
+        heading: 'Level-up your tech skills',
+  headingGradientText: '',
+  description:
+    'A10-weeks program that helps new developers gain real world experience by building real projects in a collaborative remote environment.',
+   headerImage:right_image,
     }
 }
 
