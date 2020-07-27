f131470d1069ac6da88c22a9856350600f49649c
AbdoMagdy <abdoman46@hotmail.com>
2020-07-27 19:23:42 +0200

    change pragarph to match design

--- frontend/stories/ApplyPage.stories.js
+++ frontend/stories/ApplyPage.stories.js
@@ -19,7 +19,7 @@ const data = {
     heading: 'One more step  to ',
     headingGradientText: 'Join Us.',
     description:
-      'A10-weeks program that helps new developers gain real world experience by building real projects in a collaborative remote environment.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie aliquam iaculis facilisis',
     image:
       'https://s3-alpha-sig.figma.com/img/c276/0ada/30e5f4ecc27eb8df318751c75c42c98c?Expires=1597017600&Signature=fFh0MYvoweW5r4HGxqR1HKKtgjR39uyn6hT0IsnXsMiVpva-DreihqEERiAZWnCvntyCtNO4dIHKGi8pI4J8QdWrRgM4jXgCGOEIGezTxXEKjcX0Ys-ONZ2lJP6M5crh64w1X5d~M~MzoQjMNe-70a3zkNLobjGYuK4b4EIfFFWwwv09hNLQzqOsAryBIO-sF7kCdDakrOOxNJUPlSowwJYrstxz9LX3xUzyif32uP~3BuWhKuqfr0eZOaoKJhcfXxhNLHR6adI41vAUIA53c-4ZBTV9xbsm2l9WI5cNsMNlpOJ5gp7KZf~JknROKme3Z3527zxqmCm-pDBSpU6ZjA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA',
     headerImage:
