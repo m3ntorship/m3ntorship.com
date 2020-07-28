9d314646a7a67fc146989c147ad0817a3176c430
AbdoMagdy <abdoman46@hotmail.com>
2020-07-28 18:07:43 +0200

    center the image in the form

--- frontend/components/shared/form.jsx
+++ frontend/components/shared/form.jsx
@@ -47,6 +47,7 @@ const Form = ({ inputFields }) => {
           <img
             src="https://s3-alpha-sig.figma.com/img/2b73/7a06/63d6c70e5a3acaea8b40ac2bc166be42?Expires=1596412800&Signature=g2aHFQjxXo-lKLl~mUB3J34Nx9U9VwKnTXTFnYYqJtMllS-k8H6~TGwJ71uIPrMmzubkddOCCHcSONnZMXO~HzeaD1NG~lDjgeIJMekh9sibKiueQGnZlnrU995Cf0tOxwp8vpWCEdFyoRwdhWVafdX~Sd17fejv-nKCXdVJ2aDtBJgw1xtQjJgZPmY96yd8PCUaTxMZhf8ZSKAtVj9A7tkerPwo6eI5-8WqQnHsYR7SK2seki3fe5Ff84kh9mIlRWPaNhk0bqPNkOOt05tTa5KDU8unBiXon0DzFS01CuctFJDJO-IU1ShSmR-qpy~dLUiRvbeISFPcvsWNTt5AKQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
             alt=""
+            className="mx-auto"
           />
         </div>
       </div>
