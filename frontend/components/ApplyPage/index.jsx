132a280248a4ced2bb213976e348aaf5a2479b8d
AbdoMagdy <abdoman46@hotmail.com>
2020-07-28 19:47:15 +0200

    add apply section to the page and use context in it

--- frontend/components/ApplyPage/index.jsx
+++ frontend/components/ApplyPage/index.jsx
@@ -3,41 +3,42 @@ import { UserProvider } from '../../context/UserContext';
 import { TopBar } from '../TopBar';
 import SectionHeader from '../shared/SectionHeader';
 import Footer from '../footer';
-
+import Apply from '../apply/index';
 export const ApplyPage = ({ data }) => {
-  const [mentor, setMenor] = useState(true);
+  const [mentor, setMentor] = useState(false);
 
   if (data) {
     const {
       topBarData,
       mentorSectionHeaderData,
       menteeSectionHeaderData,
+      applyData,
+      applyInputFields,
       footerData
     } = data;
 
     return (
       <>
         <div className="container">
-          <UserProvider value={{ mentor, setMenor }}>
+          <UserProvider value={{ mentor, setMentor }}>
+            <TopBar
+              data={topBarData}
+              button_color={mentor ? 'blue' : 'green'}
+            />
             {mentor ? (
-              <div>
-                <TopBar data={topBarData} button_color="blue" />
-                <SectionHeader
-                  data={mentorSectionHeaderData}
-                  gradient_color="blue"
-                  headingtype="main"
-                />
-              </div>
+              <SectionHeader
+                data={mentorSectionHeaderData}
+                gradient_color="blue"
+                headingtype="main"
+              />
             ) : (
-              <div>
-                <TopBar data={topBarData} button_color="green" />
-                <SectionHeader
-                  data={menteeSectionHeaderData}
-                  gradient_color="green"
-                  headingtype="main"
-                />
-              </div>
+              <SectionHeader
+                data={menteeSectionHeaderData}
+                gradient_color="green"
+                headingtype="main"
+              />
             )}
+            <Apply data={applyData} inputFields={applyInputFields} />
           </UserProvider>
           <Footer data={footerData} />
         </div>
