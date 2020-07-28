92e86c4b63aaa6317af13cc2b82267ef56f348ea
AbdoMagdy <abdoman46@hotmail.com>
2020-07-28 18:56:32 +0200

    use container class and add state for page color

--- frontend/components/ApplyPage/index.jsx
+++ frontend/components/ApplyPage/index.jsx
@@ -1,25 +1,39 @@
-import React, { createContext } from 'react';
-
+import React, { useState } from 'react';
+import { UserProvider } from '../../context/UserContext';
 import { TopBar } from '../TopBar';
 import SectionHeader from '../shared/SectionHeader';
 import Footer from '../footer';
-const mentor = createContext(false);
-const { Provider, Consumer } = mentor;
+
 export const ApplyPage = ({ data }) => {
+  const [mentor, setMenor] = useState(true);
+
   if (data) {
-    const { topBarData, sectionHeaderData, footerData } = data;
+    const { topBarData, mentorSectionHeaderData, footerData } = data;
     return (
       <>
-        <div className="px-12">
-          <Provider value={false}>
-            <TopBar data={topBarData} />
-            <SectionHeader
-              data={sectionHeaderData}
-              gradient_color="green"
-              headingtype="main"
-            />
-            <Footer data={footerData} />
-          </Provider>
+        <div className="container">
+          <UserProvider value={{ mentor, setMenor }}>
+            {mentor ? (
+              <template>
+                <TopBar data={topBarData} button_color="blue" />
+                <SectionHeader
+                  data={mentorSectionHeaderData}
+                  gradient_color="blue"
+                  headingtype="main"
+                />
+              </template>
+            ) : (
+              <template>
+                <TopBar data={topBarData} button_color="green" />
+                <SectionHeader
+                  data={mentorSectionHeaderData}
+                  gradient_color="green"
+                  headingtype="main"
+                />
+              </template>
+            )}
+          </UserProvider>
+          <Footer data={footerData} />
         </div>
       </>
     );
