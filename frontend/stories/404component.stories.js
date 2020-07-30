56471bcc87f50ceddc371640e635387c7a2dd1c4
Tramsi <tramsi2014@gmail.com>
2020-07-30 18:47:41 +0200

    Add BackToHomeWithTitleAndImage compoenent

---
+++ frontend/stories/404component.stories.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import BackToHomeWithTitleAndImage from '../components/BackToHomeWithTitleAndImage';
+
+export default {
+  title: '404 component',
+  component: BackToHomeWithTitleAndImage
+};
+const imageUrl =
+  'https://s3-alpha-sig.figma.com/img/5a8e/e1e6/d34cef4cdd3261805554b9b1c9f3d891?Expires=1597017600&Signature=A9LKH~djxlzJoUnOueEiQSdeFuPI8tMhfwyVvzj2G-qtPiydbm6A57ldEytcNo~0NCG63eewb2DGGgG999QLbON4SsaBDPQE61rZqrTrGlJD05aIky0kkoNhynLuTLk7pS82z0n4jcvLVZFThEHICLjzD3DMzQcPWkb-IpKix4e1HtscVoXQ8VqYxR0DaSwaBSDJdadfHkp3PlrsJFZ6Z8aEjG-Ssv83ro9qE1Lwnk5TdxQ45h~CzJyHGpLXxUuJSSfy1az8csxFDyOmMdkYc2ZgaEccCk2AdGKM4bIkb-7VQEasKG4d-ZAeKwpvDeXOGZhaLcPYWYqWyT03HxUfEA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA';
+const title = "404... You're Lost!";
+const description =
+  ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie aliquam iaculis facilisis sit pharetra pellentesque ullamcorper. ';
+const btn = {
+  text: 'GO TO HOME',
+  url: '/'
+};
+export const Custom404Component = () => {
+  return (
+    <BackToHomeWithTitleAndImage
+      imageUrl={imageUrl}
+      title={title}
+      description={description}
+      btn={btn}
+    />
+  );
+};
