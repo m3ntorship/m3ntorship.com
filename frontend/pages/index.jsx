9cf8e7aadcd123ba10ceb1e2c22ea34dc693d707
MAlngaawy <m.alngaawy@gmail.com>
2020-08-07 18:09:37 +0200

    add home page at index.jsx file

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,11 +1,101 @@
-const Home = () => {
-  return <div className="text-ultra text-c200">Hello World</div>;
-};
+import React from 'react';
+import SectionHeader from '../components/shared/SectionHeader';
+import Button from '../components/shared/Button';
+import Goals from '../components/Goals';
+import HowItWork from '../components/how-it-work';
+import Patches from '../components/Patches'
+import {mentorshipAPI} from '../clients'
 
-export async function getServerSideProps(context) {
-  return {
-    props: {},
-  };
+export const Home = ({data}) => {
+    if(data) {
+      const {home_header,
+        goals,
+        steps,
+        patches,
+        contribute} = data
+        return (
+            <>
+            <SectionHeaderComponent data={home_header} />
+            <Goals data={goals} />
+            <HowItWork data={steps} />
+            <Patches data={patches} />
+            <ContributeSection data={contribute} />
+            </>
+        )
+    }
+}
+
+// side components
+const SectionHeaderComponent = ({data}) => {
+    return (
+        <SectionHeader data={data}> 
+            <Button textColor='black' bgColor='green' btnPadding='small' textSize='medium' customClassName='uppercase' > Apply As A Member </Button>
+            <Button textColor='white' bgColor='blue' btnPadding='small' textSize='medium' customClassName='uppercase mt-2 md:ml-2 md:mt-0'> Apply As A Mentor </Button>
+        </SectionHeader>
+    )
+}
+
+const ContributeSection = ({data}) => {
+    return (
+        <SectionHeader data={data} gradient_color='blue' headingtype='section'>
+            <Button textColor='white' bgColor='blue' btnPadding='small' textSize='medium' customClassName='uppercase'> Apply As a Mentor </Button>
+        </SectionHeader>
+    )
+}
+
+
+export async function getStaticProps(context) {
+  const endPoints = [
+    mentorshipAPI('/home-header'),
+    mentorshipAPI('/goals'),
+    mentorshipAPI('/steps'),
+    mentorshipAPI('/patches'),
+    mentorshipAPI('/contribute')
+  ];
+  return Promise.all(
+    endPoints
+      .map(ep =>
+        ep
+          .then(res => {
+            if (Object.keys(res.data).length) {
+              return res;
+            } else {
+              return {
+                data: {
+                  statusCode: 404
+                }
+              };
+            }
+          })
+          .catch(err => {
+            data: {
+              message: err.message;
+            }
+          })
+      )
+      .then(
+        ([
+          { data: home_header },
+          { data: goals },
+          { data: steps },
+          { data: patches },
+          { data: contribute }
+        ]) => {
+          return {
+            props: {
+              data: {
+                home_header,
+                goals,
+                steps,
+                patches,
+                contribute
+              }
+            },
+            revalidate: 1
+          };
+        }
+      )
+  );
 }
 
 export default Home;
