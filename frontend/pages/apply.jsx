1cee19bff665401e548aa85298d53dec44484796
AbdoMagdy <abdoman46@hotmail.com>
2020-08-07 17:26:29 +0200

    add apply page in nextJs

---
+++ frontend/pages/apply.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { ApplyPage } from '../components/ApplyPage';
+import { mentorshipAPI } from '../clients/index';
+const Apply = ({ data }) => {
+  return <ApplyPage data={data} />;
+};
+
+export async function getStaticProps(context) {
+  const endPoints = [
+    mentorshipAPI('/top-bar'),
+    mentorshipAPI('/apply-page-intro-section'),
+    mentorshipAPI('/apply-page-form'),
+    mentorshipAPI('/footer')
+  ];
+  return Promise.all(
+    endPoints
+      .map(ep =>
+        ep
+          .then(res => {
+            if (Object.keys(res.data).length) {
+              return res;
+            } else {
+              return {
+                data: {
+                  statusCode: 404
+                }
+              };
+            }
+          })
+          .catch(err => {
+            data: {
+              message: err.message;
+            }
+          })
+      )
+      .then(
+        ([
+          { data: topBarData },
+          { data: headerSectionData },
+          { data: formData },
+          { data: footerData }
+        ]) => {
+          return {
+            props: {
+              data: {
+                topBarData,
+                headerSectionData,
+                formData,
+                footerData
+              }
+            },
+            revalidate: 1
+          };
+        }
+      )
+  );
+}
+
+export default Apply;
