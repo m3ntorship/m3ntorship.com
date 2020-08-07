0544c83b1fd4f2a1ea601e40119e117a516ea521
elhaw <abd.elsayed@hotmail.com>
2020-08-07 17:35:46 +0400

    consume data in batches data

--- frontend/components/Patches/index.jsx
+++ frontend/components/Patches/index.jsx
@@ -2,23 +2,27 @@ import React from 'react';
 import { GradientText } from '../shared/Heading/index';
 import PatchCard from '../patch-card';
 
-const Patches = ({ data }) => {
-  const { title, paragraph, cards, dashedCardDetails } = data;
+const Patches = ({ data, btachesHeaderData }) => {
+  const { title, description } = btachesHeaderData;
+
   if (data) {
     return (
       <div className="patches my-8">
         <div className="container">
-            <div className='text-center'>
-          <GradientText text={title} className="uppercase font-bold text-xxlg inline-block"  />
-            </div>
-          <p className='my-16 mx-auto text-center text-base text-c600 lg:w-4/6'>{paragraph}</p>
+          <div className="text-center">
+            <GradientText
+              text={title}
+              className="uppercase font-bold text-xxlg inline-block"
+            />
+          </div>
+          <p className="my-16 mx-auto text-center text-base text-c600 lg:w-4/6">
+            {description}
+          </p>
           <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 mt-10">
-            {cards.map(card => {
-              return (
-                <PatchCard cardDetails={card.cardDetails} dashed={false} />
-              );
+            {data.map((card,index) => {
+              return <PatchCard key ={index} cardDetails={card} dashed={false} />;
             })}
-            <PatchCard cardDetails={dashedCardDetails} dashed={true} />
+            {/* <PatchCard cardDetails={dashedCardDetails} dashed={true} /> */}
           </div>
         </div>
       </div>
