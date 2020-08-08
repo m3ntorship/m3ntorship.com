31f47321910c392dbb68425698b03795e59511f0
AbdoMagdy <abdoman46@hotmail.com>
2020-08-08 19:38:21 +0200

    match props with icoming data

--- frontend/components/ApplyPage/index.jsx
+++ frontend/components/ApplyPage/index.jsx
@@ -11,28 +11,36 @@ export const ApplyPage = ({ data }) => {
     const { topBarData, headerSectionData, formData, footerData } = data;
     let menteeSectionHeaderData = headerSectionData;
     menteeSectionHeaderData['headerImage'] =
-      menteeSectionHeaderData['menteeHeaderImage'];
+      menteeSectionHeaderData['menteeHeaderImage']['url'];
+    menteeSectionHeaderData['image'] = menteeSectionHeaderData['Image']['url'];
     let mentorSectionHeaderData = headerSectionData;
     mentorSectionHeaderData['headerImage'] =
-      mentorSectionHeaderData['mentorHeaderImage'];
+      mentorSectionHeaderData['mentorHeaderImage']['url'];
+    mentorSectionHeaderData['image'] = mentorSectionHeaderData['Image']['url'];
     return (
       <>
         <UserProvider value={{ mentor, setMentor }}>
           <TopBar data={topBarData} button_color={mentor ? 'blue' : 'green'} />
           {mentor ? (
-            <SectionHeader
-              data={mentorSectionHeaderData}
-              gradient_color="blue"
-              headingtype="main"
-            />
+            <div className="container mt-24">
+              <SectionHeader
+                data={mentorSectionHeaderData}
+                gradient_color="blue"
+                headingtype="main"
+              />
+            </div>
           ) : (
-            <SectionHeader
-              data={menteeSectionHeaderData}
-              gradient_color="green"
-              headingtype="main"
-            />
+            <div className="container mt-24">
+              <SectionHeader
+                data={menteeSectionHeaderData}
+                gradient_color="green"
+                headingtype="main"
+              />
+            </div>
           )}
-          <Apply data={formData} />
+          <div className="container mt-24 mb-24">
+            <Apply data={formData} />
+          </div>
         </UserProvider>
         <Footer data={footerData} />
       </>
