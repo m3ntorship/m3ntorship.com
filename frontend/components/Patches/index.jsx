16a3c3087c41c8447ec656a8a95ff12f74e5ce37
MAlngaawy <m.alngaawy@gmail.com>
2020-08-08 13:11:37 +0200

    edit batches destructure values

--- frontend/components/Patches/index.jsx
+++ frontend/components/Patches/index.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { GradientText } from '../shared/Heading/index';
 import PatchCard from '../patch-card';
 
-const Patches = ({ data }) => {
-  const { title, description, cards, dashed_card } = data;
-  if (data) {
+const Patches = ({ data, batchesCards }) => {
+  console.log('cards' , batchesCards)
+  const { title, description, dashed_card } = data;
+  if (data, batchesCards) {
     return (
       <div className="patches my-8">
         <div className="container">
@@ -13,9 +14,9 @@ const Patches = ({ data }) => {
             </div>
           <p className='my-16 mx-auto text-center text-base text-c600 lg:w-4/6'>{description}</p>
           <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 mt-10">
-            {cards.map(card => {
+            {batchesCards.map(card => {
               return (
-                <PatchCard cardDetails={card.cardDetails} dashed={false} />
+                <PatchCard cardDetails={card} dashed={false} />
               );
             })}
             <PatchCard cardDetails={dashed_card} dashed={true} />
