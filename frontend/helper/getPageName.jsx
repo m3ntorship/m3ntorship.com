7053afb0ace70648183e787b1d7ddf675cbcfed5
elhaw <abd.elsayed@hotmail.com>
2020-08-10 15:24:57 +0400

    remove unsed files

--- frontend/helper/getPageName.jsx
+++
@@ -1,9 +0,0 @@
-export default path => {
-  const firstBackSlashRemoved = path.slice(1);
-  const pathParts = firstBackSlashRemoved.split('/');
-  const [pageName, subPageName] = pathParts;
-  if (pathParts.length > 1) {
-    return subPageName;
-  }
-  return pageName;
-};
