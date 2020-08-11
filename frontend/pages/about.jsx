2b9abe2ff76f7ac3a5e7efe6f8fea819548bfea1
Tramsi <tramsi2014@gmail.com>
2020-08-11 23:57:53 +0200

    Changed about page to static generation instead of server side rendering

--- frontend/pages/about.jsx
+++ frontend/pages/about.jsx
@@ -2,39 +2,33 @@ import { ParallaxedHeader } from '../components/ParallaxedHeader';
 import { ParagraphWithImageBeside } from '../components/ParagraphWithImageBeside';
 import TeamGroupSection from '../components/TeamSection';
 import { mentorshipAPI } from '../clients/mentorship';
-import checkingDataError from '../helper/checkingDataError';
 const About = ({
   aboutData: { about_head, about_description },
-  TeamGroupData
+  teamGroupData
 }) => {
   return (
     <>
       <ParallaxedHeader data={about_head} />
-       <div className="container my-16">
+      <div className="container my-16">
         <ParagraphWithImageBeside data={about_description} />
       </div>
       <div className="container mb-16">
-        <TeamGroupSection data={TeamGroupData} />
-      </div> 
+        <TeamGroupSection data={teamGroupData} />
+      </div>
     </>
   );
 };
 
-export async function getServerSideProps() {
-  const layoutEndPointsArr = [
-    mentorshipAPI('/about-head'),
-    mentorshipAPI('/organization-founders')
-  ];
-  return Promise.all(checkingDataError(layoutEndPointsArr)).then(
-    ([{ data: aboutData }, { data: TeamGroupData }]) => {
-      return {
-        props: {
-          aboutData,
-          TeamGroupData
-        }
-      };
-    }
-  );
+export async function getStaticProps() {
+  const { data: aboutData } = await mentorshipAPI('/about-head');
+  const { data: teamGroupData } = await mentorshipAPI('/organization-founders');
+  return {
+    props: {
+      aboutData,
+      teamGroupData
+    },
+    revalidate: 1
+  };
 }
 
 export default About;
