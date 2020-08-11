cf937df3ac2d75c8c575b6fa163ea6136acd9f75
Tramsi <tramsi2014@gmail.com>
2020-08-12 00:12:53 +0200

    Changed 404 page to be staticly generated instead of server side rendered

--- frontend/pages/404.jsx
+++ frontend/pages/404.jsx
@@ -3,3 +3,7 @@ import CustomError from '../components/CustomError';
 export default function Custom404() {
   return <CustomError statusCode={404} />;
 }
+
+export async function getStaticProps() {
+  return { props: {} };
+}
