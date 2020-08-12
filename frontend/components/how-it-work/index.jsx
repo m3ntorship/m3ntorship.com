13f5af3cab0aacb9ab53c801e391434bc1404a7e
MAlngaawy <m.alngaawy@gmail.com>
2020-08-12 17:19:52 +0200

    fix how it works conflicts

--- frontend/components/how-it-work/index.jsx
+++ frontend/components/how-it-work/index.jsx
@@ -40,50 +40,42 @@ const HowItWork = ({ data }) => {
   const { title, description, cards } = data;
   if (data) {
     return (
-      <motion.div
+      <motion.section
+        className="text-center container relative"
         ref={ref}
-        className="text-center mt-40 mb-20"
         variants={sectionVariants}
         initial="hidden"
         animate={inView ? 'visible' : ''}
       >
-        <div className="container relative">
-          <Heading
-            as="h2"
-            type={HEADING_OPTIONS.TYPE.SECTION}
-            textAlign={HEADING_OPTIONS.TEXT_ALIGN.CENTER}
-          >
-            <GradientText text={title} className="uppercase font-bold" />
-          </Heading>
-          <p className="my-10 mx-auto text-base text-c600 lg:w-4/6">
-            {description}
-          </p>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 col-gap-5 row-gap-32 mt-32">
-            {cards.map((el, index) => {
-              return (
-                <motion.div
-                  variants={cardVariants}
-                  className="relative h-full"
-                  key={el.id}
-                >
-                  <span
-                    className="absolute block text-giant z-0 text-c400 font-bold "
-                    style={{
-                      top: '-120px',
-                      left: '50%',
-                      transform: 'translateX(-50%)',
-                      zIndex: '-1'
-                    }}
-                  >{` 0${index + 1}`}</span>
-                  <div className="z-10 bg-c000 h-full">
-                    <PersonCard cardDetails={el} boxShadow={true} />
-                  </div>
-                </motion.div>
-              );
-            })}
-          </div>
+        <Heading
+          as="h2"
+          type={HEADING_OPTIONS.TYPE.SECTION}
+          textAlign={HEADING_OPTIONS.TEXT_ALIGN.CENTER}
+        >
+          <GradientText text={title} className="uppercase font-bold" />
+        </Heading>
+        <p className="mb-10 mx-auto text-base text-c600 lg:w-4/6">
+          {description}
+        </p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 col-gap-5 row-gap-32 mt-32">
+          {cards.map((el, index) => {
+            return (
+              <motion.div
+                variants={cardVariants}
+                className="relative h-full"
+                key={el.id}
+              >
+                <span className="absolute block text-giant z-0 text-c400 font-bold transform -translate-x-1/2 -z-1 -top-30 left-1/2">{` 0${
+                  index + 1
+                }`}</span>
+                <div className="z-10 bg-c000 h-full">
+                  <PersonCard cardDetails={el} boxShadow={true} />
+                </div>
+              </motion.div>
+            );
+          })}
         </div>
-      </motion.div>
+      </motion.section>
     );
   }
 };
