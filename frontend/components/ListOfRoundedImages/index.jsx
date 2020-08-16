0516cb896aacdbabb1b0cb4fda7ba75d53e86a87
Tramsi <tramsi2014@gmail.com>
2020-08-16 10:44:55 +0200

    Used randomPosition function to generate random position for images

--- frontend/components/ListOfRoundedImages/index.jsx
+++ frontend/components/ListOfRoundedImages/index.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import randomPosition from '../../helper/randomPosition';
 
 export const ListOfRoundedImages = ({ data }) => {
+  let cache = [];
   return (
-    <div className="flex justify-center flex-wrap">
+    <div
+      className="grid grid-cols-4 grid-rows-4 gap-5"
+      style={{
+        placeItems: 'center',
+        gridTemplateAreas:
+          "'g1 g2 g3 g4' 'g5 g6 g7 g8' 'g9 g10 g11 g12' 'g13 g14 g15 g16'"
+      }}
+    >
       {data &&
         data.map(({ url }, index) => {
           return (
             <img
               key={index}
               src={url}
-              className="object-cover w-24 h-24 rounded-full m-5 mt-3"
+              className="object-cover w-24 h-24 rounded-full"
+              style={{ gridArea: `g${randomPosition(16, cache)}` }}
             />
           );
         })}
