0afeeb5e254070cbe5fab50b80ec5fff1c083c06
Abud Zohair <abud.zohair@gmail.com>
2020-08-16 17:07:58 +0200

    introduce sitemap
     - Add sitemap for static urls
    
     -  Add Sitemap for dynamic batches
    
    static siteMap: /sitemap.xml
    
    dynamic : /batches/sitemap.xml

---
+++ frontend/pages/batches/sitemap.xml.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const SiteMap = () => {
+  return null;
+};
+
+export default SiteMap;
+
+export async function getServerSideProps({ res }) {
+  const { data: batches } = await axios.get(`${process.env.url}/batches`);
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(createSitemap(batches));
+  res.end();
+
+  return { props: {} };
+}
+
+const createSitemap = batches => {
+  let batchesXml = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${batches
+          .map(({ batch_slug, updatedAt }) => {
+            return `
+                    <url>
+                        <loc>${`https://m3ntorship.com/batches/${batch_slug}`}</loc>
+                        <lastmod>${updatedAt}</lastmod>
+                    </url>
+                `;
+          })
+          .join('')}
+    </urlset>
+    `;
+  return batchesXml;
+};
