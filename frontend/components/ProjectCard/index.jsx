14bfbb485ead57d2fde4d29b2f10fe9c9d96bea9
MAlngaawy <m.alngaawy@gmail.com>
2020-08-17 11:54:03 +0200

    use Heading component at card

--- frontend/components/ProjectCard/index.jsx
+++ frontend/components/ProjectCard/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { HEADING_OPTIONS, Heading } from '../shared/Heading';
 
 const ProjectCard = ({ data }) => {
   if (data) {
     return (
       <div className="project-card bg-c1200 p-10">
-        <div className="my-10">
-          <h2 className="inline-block">Title</h2>
-          <div className='inline-block float-right'>
-            <a href="#" className='font-bold'>More</a>
+        <div className="my-10 flex items-center justify-center">
+          <Heading
+            type={HEADING_OPTIONS.TYPE.CARD}
+            fontWeight={HEADING_OPTIONS.FONT_WEIGHT.BOLD}
+            className="inline-block card-heading"
+          >
+            Title
+          </Heading>
+          <div className="inline-block float-right">
+            <a href="#" className="font-bold">
+              More
+            </a>
           </div>
         </div>
         <p className="description my-20 text-c600">
