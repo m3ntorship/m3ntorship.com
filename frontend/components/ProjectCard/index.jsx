65214fbb86b0b0953c90a22f237978ffac95b988
MAlngaawy <m.alngaawy@gmail.com>
2020-08-17 12:56:38 +0200

    import right arrow icon and edit styles

--- frontend/components/ProjectCard/index.jsx
+++ frontend/components/ProjectCard/index.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import { HEADING_OPTIONS, Heading } from '../shared/Heading';
+import rightArrow from '../../public/static/images/right-arrow.svg';
 
 const ProjectCard = ({ data }) => {
   if (data) {
     return (
-      <div className="project-card bg-c1200 p-10">
+      <div className="project-card bg-c1200 p-8">
         <div className="my-10 flex items-center justify-center">
           <Heading
             type={HEADING_OPTIONS.TYPE.CARD}
             fontWeight={HEADING_OPTIONS.FONT_WEIGHT.BOLD}
-            className="inline-block card-heading"
+            className="inline-block card-heading w-1/2"
           >
             Title
           </Heading>
-          <div className="inline-block float-right">
-            <a href="#" className="font-bold">
+          <div className="inline-block text-right w-1/2 mb-6">
+            <a href="#" className="font-bold inline-block mr-2">
               More
             </a>
+            <img className="inline-block w-4" src={rightArrow}></img>
           </div>
         </div>
-        <p className="description my-20 text-c600">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        <p className="description my-10 text-sm text-c600">
+          A job listings and job application app for working abroad.
         </p>
       </div>
     );
