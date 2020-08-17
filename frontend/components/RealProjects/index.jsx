856c0d1d9d0058579c0115257624f4625351a292
MAlngaawy <m.alngaawy@gmail.com>
2020-08-17 17:24:44 +0200

    implement Heading component the right way

--- frontend/components/RealProjects/index.jsx
+++ frontend/components/RealProjects/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ProjectCard from '../ProjectCard';
-import { GradientText, HEADING_OPTIONS, Heading } from '../shared/Heading';
+import { GradientText, Heading, HEADING_OPTIONS } from '../shared/Heading';
 
 const RealProjects = ({ data }) => {
   if (data) {
@@ -8,15 +8,18 @@ const RealProjects = ({ data }) => {
       <div className="real-projects">
         <div className="container">
           <div>
-            <Heading>
-              <GradientText>Real projects in Real teams</GradientText>
+            <Heading
+              type={HEADING_OPTIONS.TYPE.SECTION}
+              textAlign={HEADING_OPTIONS.TEXT_ALIGN.CENTER}
+            >
+              <GradientText text="Real projects in Real teams" />
             </Heading>
-            <p className="text-center my-10 mx-auto text-base text-c600 md:w-2/3">
+            <p className="text-center mx-auto text-base text-c600 md:w-2/3">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie
               aliquam iaculis facilisis sit pharetra pellentesque ullamcorper.
             </p>
           </div>
-          <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+          <div className="my-10 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
             <ProjectCard data="123" />
             <ProjectCard data="123" />
             <ProjectCard data="123" />
