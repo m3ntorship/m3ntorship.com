79cd2fac3c653ad0ec3d2014ab5a95ea3f3ef853
MAlngaawy <m.alngaawy@gmail.com>
2020-08-18 10:01:20 +0200

    add section and projectCard animation

--- frontend/components/RealProjects/index.jsx
+++ frontend/components/RealProjects/index.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
 import ProjectCard from '../ProjectCard';
 import { GradientText, Heading, HEADING_OPTIONS } from '../shared/Heading';
+import { motion } from 'framer-motion';
+
+const projectsSectionVariants = {
+  start: {
+    opacity: 0,
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 }
+  },
+  end: {
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      staggerChildren: 0.3
+    }
+  }
+};
+
+const projectsCardVariants = {
+  start: {
+    scale: 0
+  },
+  end: {
+    scale: 1,
+    transition: {
+      duration: 1,
+      type: 'spring'
+    }
+  }
+};
+
+const dataTest = [0, 1, 2];
 
 const RealProjects = ({ data }) => {
   if (data) {
     return (
       <section className="real-projects">
-        <div className="container">
+        <motion.div
+          variants={projectsSectionVariants}
+          initial="start"
+          animate="end"
+          className="container"
+        >
           <div>
             <Heading
               type={HEADING_OPTIONS.TYPE.SECTION}
@@ -21,11 +56,15 @@ const RealProjects = ({ data }) => {
             </p>
           </div>
           <div className="my-10 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
-            <ProjectCard data="123" />
-            <ProjectCard data="123" />
-            <ProjectCard data="123" />
+            {dataTest.map(project => {
+              return (
+                <motion.div variants={projectsCardVariants}>
+                  <ProjectCard data="0" />
+                </motion.div>
+              );
+            })}
           </div>
-        </div>
+        </motion.div>
       </section>
     );
   }
