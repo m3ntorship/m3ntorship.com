7a3a841e4dab013272160e2fd9ff796f4104da33
MAlngaawy <m.alngaawy@gmail.com>
2020-08-18 13:22:13 +0200

    use the real information as the API

--- frontend/components/ProjectCard/index.jsx
+++ frontend/components/ProjectCard/index.jsx
@@ -4,6 +4,7 @@ import rightArrow from '../../public/static/images/right-arrow.svg';
 
 const ProjectCard = ({ data }) => {
   if (data) {
+    const {project_overview : {summary}, project_name, nav_to_project_page_text, project_slug} = data
     return (
       <div className="project-card bg-c1200 p-8">
         <div className="flex items-center justify-center">
@@ -12,17 +13,17 @@ const ProjectCard = ({ data }) => {
             fontWeight={HEADING_OPTIONS.FONT_WEIGHT.BOLD}
             className="inline-block card-heading w-1/2"
           >
-            Title
+            {project_name}
           </Heading>
           <div className="inline-block text-right w-1/2 mb-6">
-            <a href="#" className="font-bold inline-block">
-              More
+            <a href={project_slug} className="font-bold inline-block">
+              {nav_to_project_page_text}
               <img className="inline-block ml-2 w-4" src={rightArrow}></img>
             </a>
           </div>
         </div>
         <p className="description my-4 text-xs text-c700">
-          A job listings and job application app for working abroad.
+          {summary}
         </p>
       </div>
     );
