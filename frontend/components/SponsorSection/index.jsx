d15cbf346b8c129ecbc7822562e172bfbc295a13
Alaa <Alaa.Saedy.eg@gmail.com>
2020-08-19 23:28:20 +0200

    merging promoting sponsors and our sponsors sections into one section. adding props to control either the button or the logos appears dynamically

--- frontend/components/SponsorSection/index.jsx
+++ frontend/components/SponsorSection/index.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import { Heading, HEADING_OPTIONS } from '../shared/Heading';
 import Button from '../shared/Button';
 
-const SponsorUs = ({ cardDesc }) => {
+const SponsorUs = ({ cardDesc, data, withBtn, withIcons }) => {
+  const { sponsorsLogos } = data;
+
+  const logosList = sponsorsLogos.map(logo => {
+    return (
+      <li className="mr-20">
+        <img src={logo.sponsorLogo} alt="" />
+      </li>
+    );
+  });
+
   return (
     <div className="py-20 border border-c400 bg-c1200">
       <div className="text-c600">
@@ -14,19 +24,28 @@ const SponsorUs = ({ cardDesc }) => {
         >
           SPONSOR
         </Heading>
-        <p className="text-base text-center">{cardDesc}</p>
+        <div className="p-2">
+          <p className="text-base text-center">{cardDesc}</p>
+        </div>
       </div>
-      <div className="mt-6">
-        <Button
-          textColor="black"
-          bgColor="green"
-          btnSize="large"
-          textSize="medium"
-          customClassName="mx-auto"
-        >
-          SPONSOR NOW
-        </Button>
-      </div>
+      {withBtn && (
+        <div className="mt-6">
+          <Button
+            textColor="black"
+            bgColor="green"
+            btnSize="large"
+            textSize="medium"
+            customClassName="mx-auto"
+          >
+            SPONSOR NOW
+          </Button>
+        </div>
+      )}
+      {withIcons && (
+        <div className="flex justify-center mt-6 p-8">
+          <ul className="flex items-center"> {logosList}</ul>
+        </div>
+      )}
     </div>
   );
 };
--- frontend/stories/SponsorUs.stories.js
+++ frontend/stories/SponsorUs.stories.js
@@ -6,8 +6,24 @@ export default {
   component: SponsorUs
 };
 
+const data = {
+  sponsorsLogos: [
+    { sponsorLogo: 'https://i.imgur.com/eBzT4Jy.png' },
+    { sponsorLogo: 'https://i.imgur.com/eCX9g0r.png' },
+    { sponsorLogo: 'https://i.imgur.com/46NXC9K.png' },
+    { sponsorLogo: 'https://i.imgur.com/J9AcZjP.png' },
+    { sponsorLogo: 'https://i.imgur.com/UhSvBgy.png' },
+    { sponsorLogo: 'https://i.imgur.com/k58X0Fy.png' }
+  ]
+};
+
 export const SponsorusShowCase = () => {
   return (
-    <SponsorUs cardDesc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie " />
+    <SponsorUs
+      cardDesc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie"
+      withBtn={true}
+      withIcons={true}
+      data={data}
+    />
   );
 };
