363417d18bd03f3e504c935856fbd521da16dd87
MAlngaawy <m.alngaawy@gmail.com>
2020-08-19 17:03:54 +0200

    check the data before rendering elements at projects card

--- frontend/components/ProjectCard/index.jsx
+++ frontend/components/ProjectCard/index.jsx
@@ -19,27 +19,27 @@ const cardVaruants = {
 };
 
 const ProjectCard = ({ data }) => {
-  if (data) {
-    const {
-      project_overview: { summary },
-      project_name,
-      nav_to_project_page_text,
-      project_slug
-    } = data;
-    const [crdRef, cardInView] = useInView({
-      threshold: 0.3,
-      triggerOnce: true
-    });
-    const isMobile = useMedia(['(min-width: 1025px)'], [false], true);
-    return (
-      <motion.div
-        ref={crdRef}
-        variants={cardVaruants}
-        initial={isMobile ? 'scale' : ''}
-        animate={isMobile && cardInView ? 'unScale' : ''}
-        className="project-card bg-c1200 p-8"
-      >
-        <div className="flex items-center justify-center">
+  const {
+    project_overview: { summary },
+    project_name,
+    nav_to_project_page_text,
+    project_slug
+  } = data;
+  const [crdRef, cardInView] = useInView({
+    threshold: 0.3,
+    triggerOnce: true
+  });
+  const isMobile = useMedia(['(min-width: 1025px)'], [false], true);
+  return (
+    <motion.div
+      ref={crdRef}
+      variants={cardVaruants}
+      initial={isMobile ? 'scale' : ''}
+      animate={isMobile && cardInView ? 'unScale' : ''}
+      className="project-card bg-c1200 p-8"
+    >
+      <div className="flex items-center justify-center">
+        {project_name && (
           <Heading
             type={HEADING_OPTIONS.TYPE.CARD}
             fontWeight={HEADING_OPTIONS.FONT_WEIGHT.BOLD}
@@ -47,17 +47,24 @@ const ProjectCard = ({ data }) => {
           >
             {project_name}
           </Heading>
+        )}
+        {project_slug && nav_to_project_page_text && (
           <div className="inline-block text-right w-1/2 mb-6">
             <a href={project_slug} className="font-bold inline-block">
               {nav_to_project_page_text}
-              <img className="inline-block ml-2 w-4" src='/static/images/right-arrow.svg'></img>
+              <img
+                className="inline-block ml-2 w-4"
+                src="/static/images/right-arrow.svg"
+              ></img>
             </a>
           </div>
-        </div>
+        )}
+      </div>
+      {summary && (
         <p className="description my-4 text-xs text-c700">{summary}</p>
-      </motion.div>
-    );
-  }
+      )}
+    </motion.div>
+  );
 };
 
 export default ProjectCard;
