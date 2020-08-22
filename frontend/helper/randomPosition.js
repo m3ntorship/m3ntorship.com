5228282ce7eb81556540bbbbf488ddaabbe14be9
Tramsi <tramsi2014@gmail.com>
2020-08-22 14:46:29 +0200

    Added random position helper function

--- frontend/helper/randomPosition.js
+++ frontend/helper/randomPosition.js
@@ -3,34 +3,33 @@ const rng = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-//Checking if object is in array of objects
-const isObjectInArray = (obj, arr) => {
-  arr.forEach(element => {
-    if (JSON.stringify(element) === JSON.stringify(obj)) {
-      return true;
-    }
-  });
-  return false;
-};
-
 //Generating random position
-export default function randomPosition(numberOfPositions, cache, margin = 0) {
-  let position = { x: rng(1, numberOfPositions), y: rng(1, numberOfPositions) };
-  if (cache.length > (numberOfPositions + 2 * margin) ** 2) {
-    return 0;
-  }
-  if (isObjectInArray(position, cache)) {
-    return randomPosition(numberOfPositions, cache);
-  } else {
-    for (let i = -margin; i <= margin; i++) {
-      for (let j = -margin; j <= margin; j++) {
-        let pos = { x: position.x + i, y: position.y + j };
-        if (!isObjectInArray(pos, cache)) {
-          cache.push(pos);
-        }
+export default function randomPosition(
+  maxX,
+  maxY,
+  radius,
+  cache = [],
+  margin = 0
+) {
+  let position = {
+    x: rng(radius, maxX - radius),
+    y: rng(radius, maxY - radius)
+  };
+  if (cache.length > 0) {
+    for (let i = 0; i < cache.length; i++) {
+      let distance = Math.hypot(
+        position.x - cache[i].x,
+        position.y - cache[i].y
+      );
+      if (distance < 2 * radius + margin) {
+        position = {
+          x: rng(radius, maxX - radius),
+          y: rng(radius, maxY - radius)
+        };
+        i = -1;
       }
     }
-    console.log(position);
-    return position;
   }
+  cache.push(position);
+  return { x: position.x - radius, y: position.y - radius };
 }
