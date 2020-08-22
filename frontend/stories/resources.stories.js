a29bb73bf567616dd62d3374020c6df80f3c2d49
AbdoMagdy <abdoman46@hotmail.com>
2020-08-22 17:38:07 +0200

    add resources section

---
+++ frontend/stories/resources.stories.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Resources from '../components/resources/indes';
+
+export default {
+  title: 'Resources',
+  component: Resources
+};
+const data = [
+  {
+    title: 'front end project',
+    url: 'google.com',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie aliquam iaculis facilisis sit pharetra pellentesque ullamcorper.'
+  },
+  {
+    title: 'front end project',
+    url: 'google.com',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie aliquam iaculis facilisis sit pharetra pellentesque ullamcorper.'
+  },
+  {
+    title: 'front end project',
+    url: 'google.com',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie aliquam iaculis facilisis sit pharetra pellentesque ullamcorper.'
+  },
+  {
+    title: 'front end project',
+    url: 'google.com',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie aliquam iaculis facilisis sit pharetra pellentesque ullamcorper.'
+  }
+];
+export const ResourcesComponenet = () => {
+  return (
+    <>
+      <div className="h-72"></div>
+      <div className="h-72"></div>
+      <Resources title="resources" resourcesCards={data} />
+    </>
+  );
+};
