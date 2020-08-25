d4e7b6a4614a0d7df81788fbc80dbbde5ec6dc9e
Mohammed Alngaawy <m.alngaawy@gmail.com>
2020-08-25 23:20:01 +0200

    add google analytics code  (#292)
    * add google analytics code to _document file
    
    * add dangerouslySetInnerHTML
    
    * added google analytics tracking code
    
    Co-authored-by: elhaw <abd.elsayed@hotmail.com>

--- frontend/pages/_document.jsx
+++ frontend/pages/_document.jsx
@@ -8,7 +8,6 @@ class MyDocument extends Document {
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-
           <meta property="og:site_name" content="M3ntorship" />
           <meta property="og:type" content="website" />
           <link
@@ -16,6 +15,20 @@ class MyDocument extends Document {
             type="image/svg"
             href="../../static/favicon/favicon.ico"
           ></link>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-175933629-1"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-175933629-1');
+        `
+            }}
+          />
         </Head>
         <body className="font-body leading-body">
           <Main />
