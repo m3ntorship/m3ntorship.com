243c53504ae3ade616a2eb2e59d218888e8e6fe4
MAlngaawy <m.alngaawy@gmail.com>
2020-08-26 20:56:31 +0200

    add useMobile animation custom hook

---
+++ frontend/helper/useMobileAnimation.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const useMobileAnimation = (settings, componentId) => {
+  const isDesktop = useMedia(['(max-width: 1024px)'], [false], true);
+  // if we are in mobile
+  if (!isDesktop) {
+    // and we enabled all animations
+    if (settings.enable_mobile_animations.all) {
+      // animate
+      return true;
+    } else {
+      // we didn't enable all animation, check the component-sepcific flag
+      return settings.enable_mobile_animations[componentId];
+    }
+  } else {
+    // we are not in mobile, then animate.
+    return true;
+  }
+};
+
+export default useMobileAnimation;
