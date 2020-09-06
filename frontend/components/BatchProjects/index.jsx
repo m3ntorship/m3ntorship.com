0aa329a5e2a0026fd0807ff06c46c4662828efc7
MAlngaawy <m.alngaawy@gmail.com>
2020-09-06 15:27:22 +0200

    increase gap

--- frontend/components/BatchProjects/index.jsx
+++ frontend/components/BatchProjects/index.jsx
@@ -68,7 +68,7 @@ const BatchProjects = ({ projectsInfoData, projectsData, settings }) => {
               </p>
             )}
           </div>
-          <div className="mt-10 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+          <div className="mt-10 grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3">
             {!projectsData.statusCode &&
               projectsData.map(project => {
                 return (
